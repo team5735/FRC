ce6148f8b7c3a8b0f707b4c8b16c8da2a457364e
Aurora Gracia <aurora_gracia@student.waylandps.org>
2026-09-02 21:13:00 -0400

    initial commit

---
+++ src/main/java/first/robot/ExampleAuto.java
@@ -0,0 +1,16 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package first.robot;
+
+import org.wpilib.opmode.Autonomous;
+import org.wpilib.opmode.OpMode;
+
+@Autonomous
+public class ExampleAuto implements OpMode {
+    public ExampleAuto(Robot robot) {
+        robot.exampleMechanism.exampleCondition.whileTrue(
+            robot.exampleMechanism.exampleCommand());
+    }
+}
