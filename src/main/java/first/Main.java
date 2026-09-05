d9f4737ec63003f9bc84ac809acfbd62f19592fa
Aurora Gracia <aurora_gracia@student.waylandps.org>
2026-09-05 10:30:20 -0400

    (format.java.sh) clang-format 1 file(s)

--- src/main/java/first/Main.java
+++ src/main/java/first/Main.java
@@ -12,8 +12,7 @@ import org.wpilib.framework.RobotBase;
  * the parameter class to the startRobot call.
  */
 public final class Main {
-    private Main() {
-    }
+    private Main() {}
 
     /**
      * Main initialization function. Do not perform any initialization here.
